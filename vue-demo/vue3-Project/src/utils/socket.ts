d80ddbdd2cd913a9ecd9af083b1d7f480c176d92
zhanglihui <lihui.zhang1@pactera.com>
2024-03-05 17:42:01 +0900

    Revert "Changes"
    This reverts commit 6b8a6f1c339a01eb7a13804696abf65d5b58e96d.

--- vue-demo/vue3-Project/src/utils/socket.ts
+++ vue-demo/vue3-Project/src/utils/socket.ts
@@ -1,15 +1,71 @@
-export const registerSockets = (sockets, proxy) => {
-    sockets &&
-    Object.keys(sockets).forEach((t) => {
-        "subscribe" !== t &&
-        "unsubscribe" !== t &&
-        proxy.$socket.emitter.addListener(t, sockets[t], proxy);
-    });
-};
+export default class Socket {
+    // cb: 存储回调函数
+    constructor(url: string, cb: (data: any) => void) {
+        this.url = url;
+        this.cb = cb;
+        this.connect();
+    }
 
-export const destroySockets = (sockets, proxy) => {
-    sockets &&
-    Object.keys(sockets).forEach((t) => {
-        proxy.$socket.emitter.removeListener(t, proxy);
-    });
-};
+    private readonly url: string = '';
+    private cb?: (data: any) => void;
+    // 和服务端连接的socket对象
+    private ws?: WebSocket;
+    private remainPayload: any[] = [];
+
+    public connect() {
+        return new Promise<void>(resolve => {
+            const ws = new WebSocket(this.url);
+
+            ws.onopen = e => {
+                console.info('ws open 连接成功', e);
+                this.remainPayload.forEach(payload => {
+                    ws.send(payload);
+                });
+                resolve();
+            };
+
+            // 得到服务端发送过来的数据
+            ws.onmessage = e => {
+                if (this.cb) {
+                    this.cb(e.data);
+                }
+            };
+
+            ws.onerror = e => {
+                console.error('ws error', e);
+            };
+
+            // ws.onclose = e => {
+            //   console.info('ws close', e);
+            // };
+            this.ws = ws;
+        });
+    }
+
+    public async send(payload: any) {
+        if (!payload || !this.ws) {
+            if (!this.ws) {
+                this.remainPayload.push(JSON.stringify(payload));
+            }
+            return;
+        }
+        if (this.isConnected) {
+            this.ws.send(JSON.stringify(payload));
+        } else {
+            // 后端主动断开 前端发起请求 需要在连接一次
+            await this.connect();
+            this.ws.send(JSON.stringify(payload));
+        }
+    }
+
+    private get isConnected() {
+        return this.ws && this.ws.readyState === WebSocket.OPEN;
+    }
+
+    public close() {
+        if (this.ws) {
+            this.ws.close();
+            this.ws = undefined;
+        }
+    }
+}
